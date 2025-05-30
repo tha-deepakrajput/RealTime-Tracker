6415ed8fc7192a3747b8b845f1813084c22aa7a4
tha-deepakrajput <deepak23@navgurukul.org>
2025-05-30 12:19:37 +0530

    added the user-disconnected thing

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
+const path = require("path");
 
 const PORT = 3000;
 const app = express();
@@ -9,8 +10,23 @@ const server = http.createServer(app);
 
 const io = socketio(server);
 
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", (socket) => {
+    socket.on("send-location", (data) => {
+        io.emit("receive-location", {id: socket.id, ...data});
+    });
+
+    socket.on("disconnect", () => {
+        io.emit("user-disconnected", socket.id);
+    });
+    
+    console.log("Connected");
+});
+
 app.get("/", (req, res) => {
-    res.send("Hey!");
+    res.render("index");
 });
 
 server.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
